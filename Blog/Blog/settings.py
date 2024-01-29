b53f4dd58bfc83fab51c25ca8015fe63baa5fb97
Moataz Fawzy <142129253+Moataz0000@users.noreply.github.com>
2024-01-30 01:34:17 +0200

    Update settings.py

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
 
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'motazfawzy73@gmail.com'
-EMAIL_HOST_PASSWORD = 'uyqe lzsp syff dkkb'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
